1937ffb2ea6a53cba379cea9bfc31a5f7a95f60f
Martin <requiemforadream@abv.bg>
2020-07-28 15:13:30 +0300

    a.

--- front-end/src/components/AdminPanel/CreateContent/CreateContent.js
+++ front-end/src/components/AdminPanel/CreateContent/CreateContent.js
@@ -75,6 +75,7 @@ export default function CreateContent() {
             .then(() => {
                 setTitle('')
                 setDescription('')
+                setAreTriggered(false)
             })
     }
 
